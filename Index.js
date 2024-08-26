1a7907f9edf6c6600f4277136663d2e019a6de89
ensayd1n <enesa.ts61@gmail.com>
2024-08-26 09:33:21 +0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -19,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Multer yapılandırmaları
+//muler sunucudan gelen form ları istek ile beraner gerekli dizilere kaydetmek için kullanılır
 const upload = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
